42e7fc307fb0638d3d577b0c87033e3dbed0de1e
texane <texane@debian.localdomain>
2009-10-02 15:51:49 +0000

    add adc, video

---
+++ src/adc.h
@@ -0,0 +1,19 @@
+/*
+** Made by fabien le mentec <texane@gmail.com>
+** 
+** Started on  Fri Oct  2 15:46:56 2009 texane
+** Last update Fri Oct  2 15:48:00 2009 texane
+*/
+
+
+
+#ifndef ADC_H_INCLUDED
+# define ADC_H_INCLUDED
+
+
+
+unsigned short adc_read(unsigned char);
+
+
+
+#endif /* ! ADC_H_INCLUDED */
--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 ** Made by fabien le mentec <texane@gmail.com>
 ** 
 ** Started on  Sun Sep 20 14:08:30 2009 texane
-** Last update Sun Sep 27 21:09:24 2009 texane
+** Last update Fri Oct  2 15:49:43 2009 texane
 */
 
 
@@ -13,6 +13,7 @@
 #include "timer.h"
 #include "config.h"
 #include "serial.h"
+#include "adc.h"
 
 
 
@@ -42,112 +43,6 @@ static void osc_setup(void)
   while (!OSCCONbits.IOFS)
     ;
 }
-
-
-/* adc module */
-
-static void adc_setup(void)
-{
-  /* an0 channel
-   */
-
-  ADCON0bits.CHS0 = 0;
-
-  /* a[0-7] analog inputs
-   */
-
-  ADCON1bits.PCFG0 = 1;
-  ADCON1bits.PCFG1 = 1;
-  ADCON1bits.PCFG2 = 1;
-  ADCON1bits.PCFG3 = 0;
-
-#if 1
-
-  /* vref-: vss, vref+: vdd
-   */
-
-  ADCON1bits.VCFG0 = 0;
-  ADCON1bits.VCFG1 = 0;
-
-#else
-
-  /* vref-: an2, vref+: an3
-   */
-
-  ADCON1bits.VCFG0 = 1;
-  ADCON1bits.VCFG1 = 1;
-
-#endif
-
-  /* result format left aligned
-   */
-
-  ADCON2bits.ADFM = 0;
-
-  /* acquisition time. osc is 8 Mhz.
-   */
-
-  ADCON2bits.ACQT0 = 1;
-  ADCON2bits.ACQT1 = 0;
-  ADCON2bits.ACQT2 = 0;
-
-  /* conversion clock: fosc / 2
-   */
-
-  ADCON2bits.ADCS0 = 0;
-  ADCON2bits.ADCS1 = 0;
-  ADCON2bits.ADCS2 = 0;
-
-  /* turn on analog module
-   */
-
-  ADCON0bits.ADON = 1;
-}
-
-
-static unsigned short adc_read(void)
-{
-  unsigned short i;
-
-  /* wait required time, for the
-     analog value to be captured
-   */
-
-  for (i = 0; i < 0x1000; ++i)
-    __asm nop __endasm;
-
-  /* start the conversion
-   */
-
-  ADCON0bits.GO = 1;
-
-  /* wait for conversion
-   */
-
-  while (!ADCON0bits.GO)
-    ;
-
-  return (((unsigned short)ADRESH << 8) | ADRESL) & 0x3ff;
-}
-
-
-#if 0
-
-static int adc_peek(unsigned short* value)
-{
-  /* if a conversion occured, get the value
-     and the
-   */
-}
-
-
-static void adc_handle_interrupt(void)
-{
-  if ()
-    ;
-}
-
-#endif
 
 
 #if 0 /* event handling */
@@ -229,8 +124,13 @@ struct move_context
     } state;
 };
 
+
 static void move_forward(void)
 {
+#ifdef USE_SERIAL
+  serial_writei(0x00);
+#endif
+
   do_pwm(1);
   do_epwm(-1);
 }
@@ -238,6 +138,10 @@ static void move_forward(void)
 
 static void move_backward(void)
 {
+#ifdef USE_SERIAL
+  serial_writei(0x01);
+#endif
+
   do_pwm(-1);
   do_epwm(1);
 }
@@ -245,6 +149,10 @@ static void move_backward(void)
 
 static void move_stop(void)
 {
+#ifdef USE_SERIAL
+  serial_writei(0x02);
+#endif
+
   do_pwm(0);
   do_epwm(0);
 }
@@ -252,6 +160,10 @@ static void move_stop(void)
 
 static void move_turn_left(void)
 {
+#ifdef USE_SERIAL
+  serial_writei(0x03);
+#endif
+
   do_pwm(1);
   do_epwm(1);
 }
@@ -259,6 +171,10 @@ static void move_turn_left(void)
 
 static void move_turn_right(void)
 {
+#ifdef USE_SERIAL
+  serial_writei(0x04);
+#endif
+
   do_pwm(-1);
   do_epwm(-1);
 }
@@ -270,11 +186,17 @@ void main(void)
 {
   osc_setup();
   int_setup();
-  srf04_setup();
+
+#ifdef USE_SERIAL
   serial_setup();
+#endif
+
+  srf04_setup();
 
  redo:
   {
+    move_forward();
+
     while (1)
       {
 #define MIN_DISTANCE_VALUE 0x0a00 /* 20 cms */
@@ -285,9 +207,15 @@ void main(void)
 
 	    move_turn_left();
 	    do_wait();
+	    do_wait();
+	    do_wait();
+	    do_wait();
+	    do_wait();
+	    do_wait();
+
 	    move_forward();
 	  }
-
+	
 	do_wait();
       }
   }
